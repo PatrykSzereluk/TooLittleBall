5f906450cb094d75408e5d4d56cebe46ab0572c5
PatrykSzereluk <patryk@boldpixel.com>
2019-03-29 14:57:23 +0100

    Added new functionality
    Added new block prefab
    Added tags and layers for gameObject
    Added graphics for blocks
    Added BlockManager scripit
    Delete unnecessary files
    Clean project

---
+++ Assets/Script/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Block : MonoBehaviour
+{
+
+    private TextMeshPro countText;
+
+
+
+
+
+}
