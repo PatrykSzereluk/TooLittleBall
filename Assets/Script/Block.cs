8e70f0951d0c532ece0073190ac175d492835a52
PatrykSzereluk <patryk@boldpixel.com>
2019-03-29 15:06:38 +0100

    Added implements for block script

--- Assets/Script/Block.cs
+++ Assets/Script/Block.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class Block : MonoBehaviour
 {
 
     private TextMeshPro countText;
+    private SpriteRenderer spriteRenderer;
 
+    private Color startColor;
 
+    public int BlockHp = 3;
 
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        countText = GetComponentInChildren<TextMeshPro>();
+        UpdateText();
+        startColor = Color.HSVToRGB(Random.Range(0.3f, 0.7f), Random.Range(0.3f, 0.7f), Random.Range(0.3f, 0.7f));
+    }
+
+    private void UpdateText()
+    {
+        countText.SetText(BlockHp.ToString());
+        spriteRenderer.color = Color.Lerp(Color.white, startColor, BlockHp / 10f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ball"))
+        {
+            BlockHp--;
+
+            if(BlockHp > 0)
+            {
+                UpdateText();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
 
 
 }
